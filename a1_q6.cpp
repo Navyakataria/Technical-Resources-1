dc3dcf7cf9a18d0f06f77f267707e8316c133e6a
Navya Kataria <120239183+Navyakataria@users.noreply.github.com>
2023-01-16 18:58:36 +0530

    submitting Q6 in assignment 1

---
+++ a1_q6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+int r,n;
+cout<<"enter n";
+cin>>n;
+while(n>0)
+{
+    r=n%10;
+    n=n/10;
+    cout<<r;
+}
+
+return 0;
+}
