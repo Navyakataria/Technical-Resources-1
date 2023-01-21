3d4a0a135e7cafe16640aefe26d59fc89c7ff9f9
Navya Kataria <120239183+Navyakataria@users.noreply.github.com>
2023-01-21 13:02:52 +0530

    submitting Q3 of assignment 2

---
+++ a2_q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int n;
+    cin >> n;
+    while(i<=n){
+        int k=1;
+        while(k<=n-i){
+            cout << " ";
+            k++;
+        }
+        int j=1;
+        while(j<=i){
+            cout << "*" ;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
